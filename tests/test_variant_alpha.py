193ced1b986b826b39650b90b3894b52e1e6f6dc
alter <altereg0@users.com>
2018-07-20 20:34:34 +0300

    Some prettify

---
+++ tests/test_variant_alpha.py
@@ -0,0 +1,42 @@
+# -----------------------------------------------------------------
+# unittest
+# -----------------------------------------------------------------
+from tests.helpers import app
+
+
+class TestBasicResource(app.AppTestCase):
+    def test_can_create(self):
+        req = self.post_json(
+            '/scores',
+            {
+                'username': 'test',
+                'company': 'test',
+                'score': 100
+            }
+        )
+        self.assertEqual(req.status_code, 201)
+        self.assertEqual(req.json.get('id'), 1)
+
+    def test_can_list(self):
+        post_req = self.post_json(
+            '/scores',
+            {
+                'username': 'my_user',
+                'company': 'test',
+                'score': 50
+            }
+        )
+        self.assertEqual(post_req.status_code, 201)
+
+        get_req = self.get('/scores')
+        self.assertEqual(get_req.status_code, 200)
+
+        score_list = get_req.json.get('scores')
+        self.assertEqual(len(score_list), 1)
+
+    def test_api(self):
+        get_req = self.get('/api')
+        self.assertEqual(get_req.status_code, 200)
+
+        body = get_req.text
+        self.assertEqual(body, 'Server works!')
