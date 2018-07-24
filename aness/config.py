44b9e2f4036c9948062eb01b39db526c528a562c
alter <altereg0@users.com>
2018-07-24 23:38:42 +0300

    Restructure config file

--- aness/config.py
+++ aness/config.py
@@ -1,4 +1,5 @@
 from aumbry import Attr, YamlConfig
+from datetime import timedelta
 
 
 class SocialOAuthConfig(YamlConfig):
@@ -23,12 +24,30 @@ class DatabaseWrapperConfig(YamlConfig):
     connection = ''
 
 
+class SecurityConfig(YamlConfig):
+    __mapping__ = {
+        'token_prefix': Attr('token_prefix', str),
+        'token_expiry': Attr('token_expiry', int),
+        'tokens_leeway': Attr('tokens_leeway', int),
+        'tokens_authorize_endpoint': Attr('tokens_authorize_endpoint', str),
+        'tokens_refresh_endpoint': Attr('tokens_refresh_endpoint', str),
+        'tokens_enable_refresh': Attr('tokens_enable_refresh', bool),
+        'secret': Attr('secret', str),
+    }
+    secret = 'secret'
+    token_prefix = 'JWT'
+    token_expiry = timedelta(hours=1)
+    tokens_leeway = timedelta(seconds=0)
+    tokens_enable_refresh = False
+
+
 class AppConfig(YamlConfig):
     __mapping__ = {
         'db': Attr('db', DatabaseWrapperConfig),
         'gunicorn': Attr('gunicorn', dict),
         'wsgiref': Attr('wsgiref', dict),
-        'social_config': Attr('social_config', SocialOAuthConfig)
+        'social_config': Attr('social_config', SocialOAuthConfig),
+        'security': Attr('security', SecurityConfig)
     }
 
     def __init__(self):
