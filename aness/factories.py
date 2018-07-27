446079e6806238445e6e6b57b7bfa840537aaf6e
alter <altereg0@users.com>
2018-07-27 23:54:48 +0300

    Затупил с запятыми, из-за этого создаваемые объекты генерились с полями типа (<mimesis_factory.MimesisField object at 0x7f778c9107f0> 0_o

--- aness/factories.py
+++ aness/factories.py
@@ -1,17 +1,21 @@
 import factory
 from mimesis_factory import MimesisField
 
-from aness.db.models import Users
+from aness.db import models
+
+class RU(MimesisField):
+    _DEFAULT_LOCALE = 'ru'
 
 class AccountFactory(factory.Factory):
     class Meta:
-        model = Users
+        model = models.Users
 
-    name = MimesisField('name',)
-    provider = MimesisField('word'),
-    uid = MimesisField('token'),
+    name = RU('name')
+    social = RU('word')
+    uid = MimesisField('token')
     # uid = factory.Sequence(lambda n: n),
     email = factory.LazyAttribute(
         lambda o: '%s@example.org' % o.name
     )
     access_token = MimesisField('token', entropy=32)
+    fusk = RU('word')
